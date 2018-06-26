f8846e2dd12c96a259f8c4e501698c858a02e2ec
marciomarquessouza <marciomultimedia@gmail.com>
2018-06-25 22:49:38 -0300

    test year

--- global_data/tests.py
+++ global_data/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import unittest
+from global_data.models import Year
 
-# Create your tests here.
+class TestBase(unittest.TestCase):
+    """
+    Basic Models Tests
+    """
+
+    def testYear(self):
+        """ Test year model result"""
+        year = Year('2018', 'Test', True)
+        self.assertTrue()
